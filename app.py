ab0fb724574aec079a56abab3d281e99c13aea81
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2023-09-18 17:29:03 +0530

    Final- status Resolved

--- app.py
+++ app.py
@@ -101,4 +101,4 @@ def filter_devices():
         return render_template('home.html', error_message="No devices found for the selected module")
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.1.8", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
